d89d8db4ce17faec612f054f06c0be75b8031dd3
Devin <devinshackle@gmail.com>
2013-12-01 16:16:47 +0000

    Basic page layout and functionality
    Basic page layout is complete.
    Color picker working for text and background colors.
    Listeners working for single example formatting.

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+$(document).ready(function() {
+
+	/**
+	*	Datums
+	**/
+	var fontsList = ['Georgia, serif',
+		'"Palatino Linotype", "Book Antiqua", Palatino, serif',
+		'"Times New Roman", Times, serif',
+		'Arial, Helvetica, sans-serif',
+		'"Arial Black", Gadget, sans-serif',
+		'"Comic Sans MS", cursive, sans-serif',
+		'Impact, Charcoal, sans-serif',
+		'"Lucida Sans Unicode", "Lucida Grande", sans-serif',
+		'Tahoma, Geneva, sans-serif',
+		'"Trebuchet MS", Helvetica, sans-serif',
+		'Verdana, Geneva, sans-serif',
+		'"Courier New", Courier, monospace',
+		'"Lucida Console", Monaco, monospace'];
+
+	//call fontsList.foreach()
+		//add an example block for each font
+			//add font name as attribute
+
+	/**
+	*	Change text color
+	**/
+	$("input[name=textcolor]").bind("change", function() {
+		var textColor = $(this).css('background-color'); 
+		$('.example').css({'color':textColor});
+	});
+
+	/**
+	*	Change text background color
+	**/
+	$("input[name=backgroundcolor]").bind("change", function() {
+		var backgroundColor = $(this).css('background-color');
+		$('.example').css({'background-color':backgroundColor});
+	});
+
+	/**
+	*	Change text size
+	**/
+	//NEED TO VALIDATE FOR NUMERIC INPUT BETWEEN 10 AND 100
+	$("input[name=textsize]").bind("change", function() {
+		var textSize = $(this).val();
+		$('.example').css({'font-size':textSize + "px"});
+	});
+
+	/**
+	*	Change text box height
+	**/
+	$("input[name=textheight").bind("change", function() {
+		var textHeight = $(this).val();
+		$('.example').css({'height':textHeight + 'px'});
+	});
+
+
+	/**
+	*	Change text box width
+	**/
+	$("input[name=textwidth").bind("change", function() {
+		var textWidth = $(this).val();
+		$('.example').css({'width':textWidth + 'px'});
+	});
+
+
+
+});
